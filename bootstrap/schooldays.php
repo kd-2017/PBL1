0331393f1047dc120d9e51e27d3109308109a0ab
ogino g <kd1176539@st.kobedenshi.ac.jp>
2017-06-05 15:51:12 +0900

    更新

---
+++ bootstrap/schooldays.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>登校日登録</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+
+<!-- Site Name -->
+<div class="sitename">
+  <div class="container">
+    <a href="#">登校日登録</a>
+  </div>
+</div>
+ 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarEexample3">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        タイトル
+      </a>
+    </div>
+    
+    <div class="collapse navbar-collapse" id="navbarEexample3">
+      <ul class="nav navbar-nav">
+        <li><a href="#">メニューＡ</a></li>
+        <li class="active"><a href="#">メニューＢ</a></li>
+        <li><a href="#">メニューＣ</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="検索キーワード">
+        </div>
+        <button type="submit" class="btn btn-default">検索</button>
+      </form>
+    </div>
+  </div>
+</nav>
